343d264bfaa5de99b4b7a29fd73d480974479059
asya-sou <asyasoughou@gmail.com>
2021-07-04 15:31:34 -0400

    Student: imageUrl, email, gpa
    imageUrl - default value
    email - cannot be empty, validate email format
    gpa - decimal X.X, set to default value 0.0

--- database/models/Student.js
+++ database/models/Student.js
@@ -11,6 +11,24 @@ const Student = db.define("student", {
   lastname: {
     type: Sequelize.STRING,
     allowNull: false
+  },
+
+  email: {
+    type: Sequelize.STRING,
+    isEmail: true,
+    allowNull: false
+  },
+
+  imageUrl: {
+    type: Sequelize.STRING,
+    isUrl: true,
+    defaultValue: 'https://lh3.googleusercontent.com/proxy/-z6dPsQgYnQVCTlBUfKhW460FRBw0CpiVIpf4RkEN8I0nWbhjfQiQqHUs0x9FSXadSGR9EPHtJeUkQ9oACWBJ6LkpKZa1_AkFTED_mfZhIQR'
+  },
+
+  gpa {
+    type: Sequelize.DECIMAL (1,1),
+    defaultValue: 0.0
+
   }
 
 });
