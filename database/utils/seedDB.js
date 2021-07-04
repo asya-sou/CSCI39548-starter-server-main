9627143bbfb31cfc9f09b01ff44a83c93fb25502
asya-sou <asyasoughou@gmail.com>
2021-07-04 15:05:44 -0400

    addd descriptions to seed campuses

--- database/utils/seedDB.js
+++ database/utils/seedDB.js
@@ -3,17 +3,19 @@ const { Campus, Student } = require('../models');
 const seedDB = async () => {
 	const dummy_campus = await Campus.create({
 		name: "Hunter College",
+		address: "123 Zoom BB Str, CUNY, NY 12345",
 		description: "This is a school in NYC."
 	});
 	const dummy_campus2 = await Campus.create({
 		name: "Harvard",
+		address: "123 Not Happening Ave, Welness Check, MO 54321",
 		description: "This is a school in MA."
 	});
 
 	const dummy_student = await Student.create({
 			firstname: "Joe",
-      lastname: "Shmo",
-		});
+      		lastname: "Shmo",
+	});
 
 	await dummy_student.setCampus(dummy_campus);
 	
